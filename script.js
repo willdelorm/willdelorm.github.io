e039e67cd306fce648b96d57e631a2385e338d43
Will Delorm <willdelorm@gmail.com>
2023-02-21 15:30:07 -0800

    add images, complete theme switcher

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const toggleSwitch = document.querySelector('input[type="checkbox"]')
-const toggleIcon = document.getElementById('toggle-icon')
-const projects = document.getElementById('projects')
-const about = document.getElementById('about')
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById("toggle-icon");
+const heroImg = document.getElementById("hero");
+const projectsHeader = document.getElementById("projects");
+const images = Array.from(document.querySelectorAll("img"));
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
 
 // Open In Progress Project in New Tab
 function openProject() {
@@ -13,42 +16,54 @@ function openLinkedIn() {
   window.open("https://linkedin.com/in/willdelorm", "_blank");
 }
 
+// Switch Theme on Images
+function switchImageTheme(theme) {
+  let newSrc = "";
+  images.forEach((img) => {
+    theme === "primary"
+      ? (img.src = img.src.replace("secondary", "primary"))
+      : (img.src = img.src.replace("primary", "secondary"));
+  });
+}
+
 // Switch Theme
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'light')
-    toggleIcon.children[0].classList.replace("fa-moon", "fa-sun")
-    localStorage.setItem('theme', 'light')
+    document.documentElement.setAttribute("data-theme", "light");
+    toggleIcon.children[0].classList.replace("fa-moon", "fa-sun");
+    switchImageTheme("secondary");
+    localStorage.setItem("theme", "light");
   } else {
-    document.documentElement.setAttribute('data-theme', '')
+    document.documentElement.setAttribute("data-theme", "");
     toggleIcon.children[0].classList.replace("fa-sun", "fa-moon");
-    localStorage.setItem('theme', 'dark')
+    switchImageTheme("primary");
+    localStorage.setItem("theme", "dark");
   }
 }
 
 // Event Listeners
-toggleSwitch.addEventListener('change', switchTheme)
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY >= 500) {
-    projects.children[0].style.backgroundColor = "var(--primary-variant)"
+    projectsHeader.children[0].style.backgroundColor = "var(--primary-variant)";
   } else {
-    projects.children[0].style.backgroundColor = "var(--background)";
+    projectsHeader.children[0].style.backgroundColor = "var(--background)";
   }
 
-  if (window.scrollY >= 1800) {
+  if (window.scrollY >= 1700) {
     about.children[0].style.backgroundColor = "var(--primary-variant)";
   } else {
     about.children[0].style.backgroundColor = "var(--background)";
   }
-})
+});
+
+toggleSwitch.addEventListener("change", switchTheme);
 
 // Check localStorage for Theme
-const currentTheme = localStorage.getItem('theme')
+const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === 'light') {
+  if (currentTheme === "light") {
     toggleSwitch.checked = true;
   }
 }
