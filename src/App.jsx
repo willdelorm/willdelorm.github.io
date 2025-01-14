e70721c04cf2cb32f9f305d919bc7b632718d934
Will Delorm <willdelorm@gmail.com>
2025-01-14 11:24:53 -0800

    add landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <main className="main">
+        <h1>Hi, my name is</h1>
+        <h2>Will Delorm.</h2>
+        <h3>Your friendly neighborhood programmer.</h3>
+        <p>
+          I'm a software engineer specializing in creating fun and engaging
+          digital platforms. Currently, I'm focused on becoming your next, best
+          hire.
+        </p>
+        <a href="#">Let's get coffee!</a>
+      </main>
+    </>
+  );
 }
 
 export default App;
